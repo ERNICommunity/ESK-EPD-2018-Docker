6c4f52d14e9be73d9e75fea80bffbbe5f5887816
Marek Linka <marek.linka@outlook.com>
2018-03-12 16:01:42 +0100

    Reorganize demos

---
+++ src/Demo2/DockerDemo/Controllers/ValuesController.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.Generic;
+using DockerDemo.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DockerDemo.Controllers
+{
+    [Route("api/[controller]")]
+    public class ValuesController : Controller
+    {
+        private readonly RedisService _redis;
+
+        public ValuesController(RedisService redis)
+        {
+            _redis = redis;
+        }
+
+        // GET api/values
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/values/5
+        [HttpGet("{key}")]
+        public string Get(string key)
+        {
+            return _redis.Get(key);
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody]string value)
+        {
+            _redis.Set("val", value);
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ src/Demo2/DockerDemo/Services/RedisService.cs
@@ -0,0 +1,25 @@
+﻿using ServiceStack.Redis;
+
+namespace DockerDemo.Services
+{
+    public class RedisService
+    {
+        private readonly IRedisClient _client;
+
+        public RedisService()
+        {
+            var manager = new RedisManagerPool("redis:6379");
+            _client = manager.GetClient();
+        }
+
+        public void Set(string key, string value)
+        {
+            _client.Set(key, value);
+        }
+
+        public string Get(string key)
+        {
+            return _client.Get<string>(key);
+        }
+    }
+}
